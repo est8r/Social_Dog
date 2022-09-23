5463cfe5f6fb20e374e8557d56615973e204cb05
est8r <estermessias6@gmail.com>
2022-09-23 19:06:53 -0300

    importando estilizações criadas

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import S from './Button.module.css';
 
 const Button = ({ texto, ...props }) => {
-  return <button {...props}>{texto}</button>;
+  return (
+    <button {...props} className={S.button}>
+      {texto}
+    </button>
+  );
 };
 
 export default Button;
