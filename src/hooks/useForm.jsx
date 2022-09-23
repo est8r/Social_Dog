826119c5f9f2a4454043864306f13a66d56b7645
est8r <estermessias6@gmail.com>
2022-09-23 19:36:46 -0300

    criação de hook useForm com regex de email

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,15 @@
+import { React, useState } from 'react';
+
+const validacoes = {
+  email: {
+    regex:
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    message: 'Preencha um email válido',
+  },
+};
+
+const useForm = () => {
+  return {};
+};
+
+export default useForm;
