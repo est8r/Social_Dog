bf5bb98f7527e9024a67dfa6ab00e9944383147c
est8r <estermessias6@gmail.com>
2022-09-23 19:01:06 -0300

    importando componente Link do router-dom para navegação

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Dogs from '../../assets/dogs.svg';
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <nav>
+        <Link to="/" aria-label="Dogs - Home"></Link>
+        <Link to="/login">Login / Criar</Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
