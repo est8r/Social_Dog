97166b40d62766ea2e016ac8923bcc00f7c66631
est8r <estermessias6@gmail.com>
2022-09-23 19:03:21 -0300

    importando estilos criados para header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import S from './Header.module.css';
 import Dogs from '../../assets/dogs.svg';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <Link to="/" aria-label="Dogs - Home">
+    <header className={S.header}>
+      <nav className={`${S.nav} container`}>
+        <Link className={S.logo} to="/" aria-label="Dogs - Home">
           <img width={50} height={50} src={Dogs} />
         </Link>
-        <Link to="/login">Login / Criar</Link>
+        <Link className={S.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
     </header>
   );
